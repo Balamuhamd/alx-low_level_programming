c20c6708a2e210b9d78ea0e838a771f8a8c49248
Balamuhamd <usmanu915091@gmail.com>
2022-09-12 03:55:54 -0700

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+
 #include <unistd.h>
-<<<<<<< HEAD
-/**
- *  * main - Entry point
- *   * Return:1
- *
-   */
-int main(void)
-{
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
-return (1);
-}}			
-=======
+
+
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+ *
+ * * main - Entry point
+ *
+ * *
+ *
+ * * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * *
+ *
+ * *  Return: Always 0 (Success)
+ *
+ * */
+
+
 
 int main(void)
 
@@ -28,4 +29,3 @@ int main(void)
 			return (1);
 
 }
->>>>>>> ecbfcbaf30eee186d3564d21105ecd859b03491e
