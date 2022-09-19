24485d5e93e68d1332f78f9d27edcfd29081b988
Balamuhamd <usmanu915091@gmail.com>
2022-09-19 06:47:14 -0700

    task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * @an: puntero
  * Return: (0)
  */
-void reset_to_989(int *n)
+void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-void _putchar (char c);
+#include <stdio.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,5 +14,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif
+int _atoi(char *s);
+	
+#endif /* MAIN_H */
