6afb5fe41e8ebdca49f66b87e9fce5cc8975d529
Balamuhamd <usmanu915091@gmail.com>
2022-10-06 05:42:54 -0700

    tasak

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (strDup == NULL)
 		return (NULL);
 	i = j = 0;
-	while (s1[i] != '\n')
+	while (s1[i] != '\0')
 	{
 		strDup[i] = s1[i];
 		i++;
